ecdd06be8bdefdef22af7abb8f45a884336d0d2a
Irumva Shumbusho <102556884+kent250@users.noreply.github.com>
2024-06-20 20:41:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,17 @@ const bodyParser = require('body-parser');
 const app = express(); // create express instance
 const port = 3000; //port where API will listen to
 
+
 //create connection object
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'todolist_fsa'
+    host: 'bn9xf3pykr8ruumy0rh2-mysql.services.clever-cloud.com',
+    user: 'uymqbeofwpld5srp',
+    password: 'ZycCBFj6o8eOnMzzE2mH',
+    database: 'bn9xf3pykr8ruumy0rh2'
 });
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); // Allow requests from your Vue app origin
+    res.setHeader('Access-Control-Allow-Origin', 'https://to-do-list-fsa-hackathon.onrender.com'); // Allow requests from your Vue app origin
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allowed HTTP methods
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allowed headers
     next();
